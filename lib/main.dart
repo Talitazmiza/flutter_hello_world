d875928d0eb36955f79755c4af2399c8e013b996
talita-izan <talitazmiza@gmail.com>
2023-03-14 13:49:16 +0700

    tugas 3 part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_hello_world/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,133 +16,141 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Talita Iza Nurazmi (3122640024) Bab 2'),
+      home: const DetailScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 1;
-  String _text = "Ganjil";
-
-  void _percobaanSatu() {
-    setState(() {
-      _counter++;
-      if(_counter>20) {
-        _counter = 1;
-      }
-    });
-  }
-
-  void _percobaanDua() {
-    setState(() {
-      _counter++;
-      if(_counter>10) {
-        _counter = 1;
-      }
-
-      if(_counter%2 == 0) {
-        _text = "Genap";
-      } else {
-        _text = "Ganjil";
-      }
-    });
-  }
-
-  void _percobaanTiga() {
-    setState(() {
-      _counter++;
-      if(_counter>10) {
-        _counter = 0;
-      }
-
-      _text = "Ganjil: ";
-      for(int i = 0; i<=_counter; i++) {
-        if(i%2 != 0) {
-          _text += '${i}, ';
-        }
-      }
-    });
-  }
-
-  void _latihan() {
-    setState(() {
-      _counter++;
-      if(_counter>20) {
-        _counter = 1;
-      }
-
-      _text = "Genap Kelipatan 3: ";
-      for(int i = 1; i<=_counter; i++) {
-        i%2 == 0 && i%3 == 0 ? _text += '${i}, ' : '';
-      }
-    });
-  }
-
-  void _tugas() {
-    setState(() {
-      _counter++;
-      _text = "Prima: ";
-      String tPrime = '';
-      for(int i=2; i<=_counter; i++){
-        for(int j=2; j<=i; j++){
-          if(j==i && i!=2) break;
-          if(i%j == 0 && i != 2){
-            tPrime = '';
-            break;
-          } else {
-            tPrime = '$i ';
-          }
-          // i%j == 0 && i != 2 ? tempPrima = '' : tempPrima = '$i ';
-        }
-        _text += tPrime;
-      }
-    });
-  }
-
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key:key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              _text,
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Row(
-              children: [
-                TextButton(onPressed: _percobaanSatu, style: TextButton.styleFrom(backgroundColor: Colors.pink), child: Text('1')),
-                TextButton(onPressed: _percobaanDua, style: TextButton.styleFrom(backgroundColor: Colors.pink), child: Text('2')),
-                TextButton(onPressed: _percobaanTiga, style: TextButton.styleFrom(backgroundColor: Colors.pink), child: Text('3')),
-                TextButton(onPressed: _latihan, style: TextButton.styleFrom(backgroundColor: Colors.pink), child: Text('latihan')),
-                TextButton(onPressed: _tugas, style: TextButton.styleFrom(backgroundColor: Colors.pink), child: Text('Tugas')),
-              ],
-            ),
-          ],
-        ),
-      ),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget> [
+            Image.asset('assets/images/submarine_1.jpg'),
+            Container(
+              margin: EdgeInsets.only(top: 16.0),
+              child: Text(
+                "Surabaya Submarine Monument",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Lobster',
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ), // Container judul
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: const <Widget>[
+                      Icon(Icons.calendar_today),
+                      Text('Open Everyday'),
+                    ],
+                  ),
+                  Column(
+                    children: const <Widget>[
+                      Icon(Icons.punch_clock_outlined),
+                      Text('08:00 - 16:00'),
+                    ],
+                  ),
+                  Column(
+                    children: const <Widget>[
+                      Icon(Icons.attach_money),
+                      Text('Rp 10.000,-'),
+                    ],
+                  )
+                ],
+              ),
+            ), // Container icons
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Imágenes libres de regalías de Museum inside a decommissioned russian war submarine with tours & an adjacent park with cafes',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontFamily: 'Oxygen',
+                    fontSize: 16.0
+                ),
+              ),
+            ), // Container deskripsi
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30.0),
+                        child: Container(
+                          alignment: Alignment.center,
+                          constraints: const BoxConstraints(
+                            maxHeight: 100,
+                            maxWidth: 150,
+                          ),
+                          color: Colors.pink,
+                          child: Image.asset('assets/images/submarine_1.jpg'),
+                        ),
+                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30.0),
+                      child: Container(
+                        alignment: Alignment.center,
+                        constraints: const BoxConstraints(
+                          maxHeight: 100,
+                          maxWidth: 150,
+                        ),
+                        color: Colors.black,
+                        child: Image.network('https://upload.wikimedia.org/wikipedia/commons/6/60/Submarine_Monument_Surabaya_2.JPG'),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30.0),
+                      child: Container(
+                        alignment: Alignment.center,
+                        constraints: const BoxConstraints(
+                          maxHeight: 100,
+                          maxWidth: 150,
+                        ),
+                        color: Colors.pink,
+                        child: Image.asset('assets/images/submarine_1.jpg'),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30.0),
+                      child: Container(
+                        alignment: Alignment.center,
+                        constraints: const BoxConstraints(
+                          maxHeight: 100,
+                          maxWidth: 150,
+                        ),
+                        color: Colors.black,
+                        child: Image.network('https://upload.wikimedia.org/wikipedia/commons/6/60/Submarine_Monument_Surabaya_2.JPG'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ), // Container images
+        ],
+      ),)
     );
-
   }
 }
